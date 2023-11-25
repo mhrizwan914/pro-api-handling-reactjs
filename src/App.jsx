16c99711c297853b9de1f386d105fb6130889a4e
Muhammad Rizwan <mhrizwan914@gmail.com>
2023-11-25 20:32:34 +0500

    Utilize Iffi Functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import React from "react";
 
 const App = () => {
   const [products, setProducts] = React.useState([]);
+  const [errors, setErrors] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
-    ; (async () => {
-      const response = await fetch(`http://localhost:3000/api/v1/products/`);
-      const result = await response.json();
-      setProducts(result);
+    (async () => {
+      try {
+        setErrors(false);
+        setLoading(true);
+        const response = await fetch(`http://localhost:3000/api/v1/products`);
+        const result = await response.json();
+        setProducts(result);
+        setLoading(false);
+      } catch (error) {
+        setErrors(true);
+        setLoading(false);
+      }
     })()
   }, [])
+
   return (
     <section>
       <div className="py-10">
@@ -17,8 +28,10 @@ const App = () => {
           <h2 className="text-xl font-bold leading-normal text-center">
             Avoiding Race Conditions
           </h2>
-          <p className="text-lg font-bold leading-normal text-center">
-            {products.length}
+          <p className="text-lg font-bold leading-normal text-center py-5 bg-green-300 mt-5">
+            {!errors && !loading && products.length}
+            {errors && ("Something went wrong")}
+            {loading && ("Loading...")}
           </p>
         </div>
       </div>
